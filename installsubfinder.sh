68461461fa4ab578032a99b0a7ce2298cf44eb5e
boutros-12 <boutroslaham12345@gmail.com>
2025-08-08 16:46:45 +0300

    Fix Subfinder download URL and add download verification

--- installsubfinder.sh
+++ installsubfinder.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 set -e
-
 SUBFINDER_VERSION="2.8.0"
-
 mkdir -p bin
-
 echo "Downloading Subfinder v${SUBFINDER_VERSION}..."
 curl -L -o subfinder-linux-amd64.zip https://github.com/projectdiscovery/subfinder/releases/download/v${SUBFINDER_VERSION}/subfinder-linux-amd64.zip
-
 echo "Downloaded file details:"
 ls -lh subfinder-linux-amd64.zip
 file subfinder-linux-amd64.zip
-
 unzip -o subfinder-linux-amd64.zip -d bin
-
 chmod +x bin/subfinder
-
 rm subfinder-linux-amd64.zip
-
 echo "Subfinder installed at ./bin/subfinder"
