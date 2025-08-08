aa43b65c5245aa29eda6d4ae0712d8b537387860
boutros-12 <boutroslaham12345@gmail.com>
2025-08-08 16:30:51 +0300

    Add installsubfinder.sh for Subfinder install and update app.py for local binary usage

--- installsubfinder.sh
+++ installsubfinder.sh
@@ -3,19 +3,19 @@ set -e
 
 SUBFINDER_VERSION="2.8.0"
 
-# Create bin folder
+# Create bin directory if not present
 mkdir -p bin
 
-# Download Subfinder release (Linux amd64)
-curl -L -o subfinder-linux-amd64.tar.gz https://github.com/projectdiscovery/subfinder/releases/download/v${SUBFINDER_VERSION}/subfinder-linux-amd64.tar.gz
+# Download the Subfinder Linux AMD64 zip archive from the official GitHub releases
+curl -L -o subfinder-linux-amd64.zip https://github.com/projectdiscovery/subfinder/releases/download/v${SUBFINDER_VERSION}/subfinder-linux-amd64.zip
 
-# Extract to ./bin
-tar -xzf subfinder-linux-amd64.tar.gz -C bin
+# Unzip the downloaded zip file into ./bin directory
+unzip -o subfinder-linux-amd64.zip -d bin
 
-# Make executable
+# Make the Subfinder binary executable
 chmod +x bin/subfinder
 
-# Remove tarball
-rm subfinder-linux-amd64.tar.gz
+# Remove the zip archive to save space
+rm subfinder-linux-amd64.zip
 
 echo "Subfinder installed at ./bin/subfinder"
